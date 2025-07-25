dfd8a2e1c5515d93dd97b032b3b2cabb4b570c11
mehkij <disismehki@gmail.com>
2025-07-25 15:17:51 -0400

    create web server w/ api routes

---
+++ handle_comments.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func (cfg *apiConfig) createCommentHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (cfg *apiConfig) getCommentHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/mehkij/revision/internal/database"
+)
+
+type apiConfig struct {
+	queries *database.Queries
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("unable to open connection to database: %s", err)
+	}
+	defer db.Close()
+
+	dbQueries := database.New(db)
+
+	apiCfg := &apiConfig{
+		queries: dbQueries,
+	}
+
+	mux := http.NewServeMux()
+
+	// API
+	mux.HandleFunc("POST /api/v1/comments", apiCfg.createCommentHandler)
+	mux.HandleFunc("GET /api/v1/comments", apiCfg.getCommentHandler)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Printf("server starting on port: %s\n", server.Addr)
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("error starting server: %s", err)
+	}
 }
