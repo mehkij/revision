080e97e556994b31be0d7507dfbe39f7e3878311
mehkij <disismehki@gmail.com>
2025-07-26 20:22:09 -0400

    install tailwindcss

--- extension/sidebar-ui/vite.config.ts
+++ extension/sidebar-ui/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "../media", // so VSCode extension can load it
     emptyOutDir: true,
