0a5237546e0a9ef28109ae1dfd655584d59c76fd
mehkij <disismehki@gmail.com>
2025-07-27 17:33:41 -0400

    globally scope auth provider isntance

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -5,17 +5,23 @@ import { CommentsViewProvider } from "./CommentsViewProvider";
 import { AuthViewProvider } from "./AuthViewProvider";
 import { URIHandler } from "./URIHandler";
 
+let authProviderInstance: AuthViewProvider | undefined;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   const commentsProvider = new CommentsViewProvider(context);
-  const authProvider = new AuthViewProvider(context);
+  const authProviderInstance = new AuthViewProvider(context);
 
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider("commentsView", commentsProvider),
-    vscode.window.registerWebviewViewProvider("authView", authProvider),
+    vscode.window.registerWebviewViewProvider("authView", authProviderInstance),
     vscode.window.registerUriHandler(new URIHandler())
   );
+}
+
+export function getAuthProvider(): AuthViewProvider | undefined {
+  return authProviderInstance;
 }
 
 // This method is called when your extension is deactivated
