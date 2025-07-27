c194befc02380821ddb37f1dbad26e091421d905
mehkij <disismehki@gmail.com>
2025-07-26 21:28:52 -0400

    create CORS middleware

---
+++ middleware.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next(w, r)
+	}
+}
