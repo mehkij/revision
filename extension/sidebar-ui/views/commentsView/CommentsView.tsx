f5c9452f5eebb85201bb7876f6a2301b875351bf
mehkij <disismehki@gmail.com>
2025-07-27 19:24:25 -0400

    update comments view

--- extension/sidebar-ui/views/commentsView/CommentsView.tsx
+++ extension/sidebar-ui/views/commentsView/CommentsView.tsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 type Comment = {
   id: string;
   author: string;
@@ -5,31 +8,55 @@ type Comment = {
   body: string;
   date: string;
   filepath: string;
+  repo: string;
+  created_at?: string;
+  resolved: boolean;
 };
 
-const MOCK_COMMENTS: Comment[] = [
-  {
-    id: "123456789",
-    author: "mehkij",
-    avatar_url: "https://avatars.githubusercontent.com/u/109314751?v=4",
-    body: "Fix fix fix",
-    date: "07-26-2025",
-    filepath: "src/main.tsx",
-  },
-  {
-    id: "987654321",
-    author: "OmarEP",
-    avatar_url: "https://avatars.githubusercontent.com/u/32280939?v=4",
-    body: "Something something something",
-    date: "07-26-2025",
-    filepath: "src/App.tsx",
-  },
-];
+const repoName = "revision";
 
 function CommentsView() {
+  const [jwtToken, setJwtToken] = useState<string | null>(null);
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const vscode = window.acquireVsCodeApi();
+
+    vscode.postMessage({ command: "requestTokens" });
+
+    window.addEventListener("message", (event) => {
+      const message = event.data;
+      if (message.command === "sendTokens") {
+        setJwtToken(message.jwt);
+
+        axios
+          .get("https://revision.duckdns.org/api/v1/comments", {
+            params: {
+              repo: repoName,
+            },
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+            },
+          })
+          .then(function (res) {
+            setComments(res.data);
+          })
+          .catch((err) => {
+            console.error("Error fetching comments:", err);
+            vscode.postMessage({
+              command: "error",
+              message: "Failed to fetch comments. Please try again later.",
+            });
+          });
+      }
+    });
+  }, [jwtToken]);
+
   return (
     <div className="flex-col">
-      {MOCK_COMMENTS.map((comment) => (
+      {comments.map((comment) => (
         // Comment card
         <div
           className="bg-gray-700 shadow-md rounded-lg border border-gray-600 p-6 mb-4 w-full max-w-md"
