fc037501606f5b3df0f25edae47a29bea320e1dd
mehkij <disismehki@gmail.com>
2025-07-28 04:38:36 -0400

    update CORS middleware

--- middleware.go
+++ middleware.go
@@ -4,8 +4,8 @@ import "net/http"
 
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Origin", "vscode-webview://revision")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		if r.Method == "OPTIONS" {
