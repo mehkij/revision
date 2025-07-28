737a9028d26c634488e9ba601da6eb8dd70690bb
mehkij <disismehki@gmail.com>
2025-07-28 04:44:17 -0400

    fix CORS policy issue

--- middleware.go
+++ middleware.go
@@ -1,10 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "vscode-webview://revision")
+		origin := r.Header.Get("Origin")
+
+		if origin != "" && strings.HasPrefix(origin, "vscode-webview://") {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
